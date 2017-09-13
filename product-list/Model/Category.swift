5f173f67a2c2041f060ac0d768c39486e920863c
Mykola Fomin <giraffe.in.ua@gmail.com>
2017-09-13 15:40:00 +0300

    Added Model and DataService. Completed app.

---
+++ product-list/Model/Category.swift
@@ -0,0 +1,9 @@
+//
+//  Category.swift
+//  product-list
+//
+//  Created by Fomin Mykola on 9/13/17.
+//  Copyright © 2017 Fomin Mykola. All rights reserved.
+//
+
+import Foundation
---
+++ product-list/Services/DataService.swift
@@ -0,0 +1,9 @@
+//
+//  DataService.swift
+//  product-list
+//
+//  Created by Fomin Mykola on 9/13/17.
+//  Copyright © 2017 Fomin Mykola. All rights reserved.
+//
+
+import Foundation
