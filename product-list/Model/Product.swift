50ceb108b0fbc1fef2500d6256cd5aaa645828d3
Mykola Fomin <giraffe.in.ua@gmail.com>
2017-09-13 16:16:49 +0300

    Added Model for Product, added dummy data in DataService for testing. Added method for retrieve products.

---
+++ product-list/Model/Product.swift
@@ -0,0 +1,21 @@
+//
+//  Product.swift
+//  product-list
+//
+//  Created by Fomin Mykola on 9/13/17.
+//  Copyright © 2017 Fomin Mykola. All rights reserved.
+//
+
+import Foundation
+
+struct Product {
+    private(set) var title: String
+    private(set) var price: String
+    private(set) var imageName: String
+    
+    init(title: String, price: String, imageName: String) {
+        self.title = title
+        self.price = price
+        self.imageName = imageName
+    }
+}
